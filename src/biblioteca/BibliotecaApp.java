34fad71278dcb28f7114b135a4fecd95a7d112a0
Caio Abreu <caio3015@hotmail.com>
2020-04-17 19:24:45 -0300

    Criação da classe livro

--- src/biblioteca/BibliotecaApp.java
+++ src/biblioteca/BibliotecaApp.java
@@ -3,7 +3,8 @@ package biblioteca;
 public class BibliotecaApp {
 
 	public static void main(String[] args) {
-
+		Livro l1 = new Livro("Java - A referencia");
+		System.out.println(l1.getTitulo());
 	}
 
 }
