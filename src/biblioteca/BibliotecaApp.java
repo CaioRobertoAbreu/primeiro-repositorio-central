ffb1858567aeaba0030cc04749cba7bc68fefc34
Caio Abreu <caio3015@hotmail.com>
2020-04-17 21:37:32 -0300

    Revert "Criação da classe emprestimo"
    This reverts commit f537e553608eb1fdcf38de7409891aa29cd601c2.

--- src/biblioteca/BibliotecaApp.java
+++ src/biblioteca/BibliotecaApp.java
@@ -5,13 +5,6 @@ public class BibliotecaApp {
 	public static void main(String[] args) {
 		Livro l1 = new Livro("Java - A referencia");
 		System.out.println(l1.getTitulo());
-		
-		Usuario caio = new Usuario();
-		
-		Emprestimo emprestimo = new Emprestimo();
-		
-		emprestimo.setLivro(l1);
-		emprestimo.setUsuario(caio);
 	}
 
 }
